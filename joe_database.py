6fd6918ccc5a7c886b45764b4f813a89e5c3e643
joe <orbitalotter1@gmail.com>
2021-08-12 14:27:23 +1200

    First iteration of printing the database

--- joe_database.py
+++ joe_database.py
@@ -1,7 +1,7 @@
 import sqlite3
 import easygui as ui
 
-conn = sqlite3.connect("CityTemperatures.db")
+conn = sqlite3.connect("films.db")
 c = conn.cursor()
 
 def sortdata():
@@ -14,7 +14,24 @@ def amend():
     None
 
 def printdb():
-    None
+    printtemp = []
+    for row in c.execute('SELECT * FROM tblFilms'):
+        printtemp.append(row)
+    for i in range(len(printtemp)):
+        printtemp[i] = ", ".join([str(x) for x in printtemp[i]])
+    ui.codebox("Here is the database", text="\n".join(printtemp))
 
 def delete():
     None
+
+ans = ""
+while ans != "Quit":
+    ans = ui.buttonbox("What do you want to do", choices=["Print", "Insert", "Amend", "Delete", "Quit"], title="Menu")
+    if ans == "Print":
+        printdb()
+    if ans == "Insert":
+        insert()
+    if ans == "Amend":
+        amend()
+    if ans == "Delete":
+        delete()
