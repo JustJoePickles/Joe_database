57d1328b6d0ac3fcc8332e92d2b944543b7c7f87
joe <orbitalotter1@gmail.com>
2021-08-12 12:52:25 +1200

    Created general component layout as functions

--- joe_database.py
+++ joe_database.py
@@ -1,1 +1,20 @@
-# hello world
+import sqlite3
+import easygui as ui
+
+conn = sqlite3.connect("CityTemperatures.db")
+c = conn.cursor()
+
+def sortdata():
+    None
+
+def insert():
+    None
+
+def amend():
+    None
+
+def printdb():
+    None
+
+def delete():
+    None
