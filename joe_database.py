3b80da7f57abc68b645a333296b1f87f50505799
joe <orbitalotter1@gmail.com>
2021-08-19 16:51:50 +1200

    First iteration of amending element completed

--- joe_database.py
+++ joe_database.py
@@ -5,30 +5,53 @@ conn = sqlite3.connect("films.db")
 c = conn.cursor()
 
 
-def sortdata():
-    None
+def sortdata(name, year, rating, runtime, genre):
+    return [str(name).title(),int(year),str(rating).upper(),int(runtime),str(genre).title()]
 
 
 def insert():
     title = ui.enterbox("Please enter the title of the movie")
-    year = ui.integerbox("Please enter the year the movie was released", lowerbound=1888,upperbound=2050)
+    year = ui.integerbox("Please enter the year the movie was released", lowerbound=1888, upperbound=2050)
     age = ui.enterbox("Please enter the age restriction on the movie")
-    runtime=ui.integerbox("Please enter the length of the film in minutes", lowerbound=0, upperbound=500)
-    genre=ui.enterbox("Please enter the genre of the film")
-    values = (str(title),int(year),str(age),int(runtime),str(genre))
+    runtime = ui.integerbox("Please enter the length of the film in minutes", lowerbound=0, upperbound=500)
+    genre = ui.enterbox("Please enter the genre of the film")
+    values = (str(title), int(year), str(age), int(runtime), str(genre))
     c.execute("INSERT INTO tblFilms(TITLE,YEAR,AGE,RUNTIME,GENRE) VALUES (?,?,?,?,?);", values)
     conn.commit()
 
 
 def amend():
-    None
+    c.execute("SELECT TITLE from tblFilms")
+    titles = c.fetchall()
+    titles = [t for x in titles for t in x]
+    title = ui.multchoicebox("Which movie do you want to amend", choices=titles, preselect=None)
+    items = ["TITLE", "YEAR", "AGE", "RUNTIME", "GENRE"]
+    for movie in title:
+        name = ui.enterbox("Please enter a new title for " + movie)
+        year = ui.integerbox("Please enter a new year of release for " + movie)
+        rating = ui.enterbox("Please enter a new age rating for " + movie)
+        runtime = ui.integerbox("Please enter a new runtime for " + movie)
+        genre = ui.enterbox("Please enter a new genre for " + movie)
+        updated_values = sortdata(name, year, rating, runtime, genre)
+        for i in range(len(items)):
+            if i == 0:
+                c.execute(
+                    "UPDATE tblFilms SET '" + items[i] + "' = '" + str(updated_values[i]) + "' where TITLE = '" + movie + "'")
+            else:
+                c.execute(
+                    "UPDATE tblFilms SET '" + items[i] + "' = '" + str(updated_values[i]) + "' where TITLE = '" + updated_values[
+                        0] + "'")
+    conn.commit()
 
 
 def showdb():
     printtemp = []
     for row in c.execute('SELECT * FROM tblFilms'):
         printtemp.append(row)
+    print(printtemp)
     for i in range(len(printtemp)):
+        printtemp[i]=list(printtemp[i])
+        printtemp[i][0]=i+1
         printtemp[i] = ", ".join([str(x) for x in printtemp[i]])
     ui.codebox("Here is the database", text="\n".join(printtemp))
 
@@ -38,9 +61,7 @@ def delete():
     films = c.fetchall()
     films = [t for x in films for t in x]
     film = ui.multchoicebox("Which city do you want to delete", choices=films, preselect=None)
-    print(films, film)
     for title in film:
-        print(title)
         if title is not None:
             c.execute("DELETE FROM tblFilms WHERE TITLE='" + title + "'")
     conn.commit()
